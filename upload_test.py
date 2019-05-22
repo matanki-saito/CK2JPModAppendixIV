66a25a8e98f80bba2bc8c70b2f4570661b0f5172
matanki.saito <matanki.saito@gmail.com>
2019-05-22 19:37:38 +0900

    ok

--- upload_test.py
+++ upload_test.py
@@ -7,6 +7,26 @@ gauth.LocalWebserverAuth()
 # Create GoogleDrive instance with authenticated GoogleAuth instance.
 drive = GoogleDrive(gauth)
 
-file1 = drive.CreateFile({'title': 'Hello.txt'})  # Create GoogleDriveFile instance with title 'Hello.txt'.
-file1.SetContentString('Hello World!') # Set content of the file from given string.
+file1 = drive.CreateFile({
+    'title': 'Hello.txt',
+    'parents': [
+        {
+            "kind": "drive#fileLink",
+            "id": '1MUdH6S6O-M_Y5jRUzNrzQ8tPZOhm_aES'
+        }
+    ]
+})  # Create GoogleDriveFile instance with title 'Hello.txt'.
+file1.SetContentString('Hello World!')  # Set content of the file from given string.
 file1.Upload()
+
+permission = file1.InsertPermission({
+    'type': 'anyone',
+    'value': 'anyone',
+    'role': 'reader'})
+
+file1.FetchMetadata()
+
+# Fetches all metadata available.
+# file1.FetchMetadata(fetch_all=True)
+
+print(file1['id'])
